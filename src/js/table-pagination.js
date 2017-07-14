e6d4ea54a86d23588644225ce12b49e29caeb939
liuxinxin <liuxinxin>
2017-07-14 15:43:14 +0800

    modify yunnex-table component

--- src/js/table-pagination.js
+++ src/js/table-pagination.js
@@ -1,8 +1,8 @@
 /**
  * Created by liuxinxin on 2017/5/18.
  */
-var Vue = require('vue').default,
-    yunnexTable = require('./components/yunnex-table'),
+var Vue = require('vue'),
+    yunnexTable = require('./components/yunnex-table/index'),
     app;
 
 app = new Vue({
@@ -22,6 +22,85 @@ app = new Vue({
             {text: '启用', value: '2'},
             {text: '暂停', value: '3'},
             {text: '过期', value: '4'}
+        ],
+        titles: [
+            {
+                name: 'appName',
+                text: function(h, params) {
+                    return h('i', {
+                        'class': 'fa fa-th-large'
+                    }, '应用名称');
+                },
+                width: '14.286%'
+            },
+            {
+                name: 'appPackage',
+                text: '应用包名',
+                width: '14.286%'
+            },
+            {
+                name: 'appType',
+                text: '应用类型',
+                width: '14.286%'
+            },
+            {
+                name: 'author',
+                text: '所属开发者',
+                width: '14.286%'
+            },
+            {
+                name: 'crateTime',
+                text: '创建日期',
+                width: '14.286%'
+            },
+            {
+                name: 'slogan',
+                text: '应用标语',
+                width: '14.286%'
+            },
+            {
+                name: 'operate',
+                text: '操作',
+                width: '14.286%',
+                render: function(h, params) {
+                    return h('div', {
+                        'class': {
+                            'yunnex-btn': true
+                        },
+                        on: {
+                            click: function() {
+                                self.updateApp();
+                            }
+                        }
+                    }, '更新');
+                }
+            }
+        ],
+        tableList: [
+            {
+                appName: '外卖',
+                appPackage: 'yunnex.waimai',
+                appType: '掌贝官方应用',
+                author: '掌贝',
+                crateTime: '2016-03-15',
+                slogan: '掌贝外卖让天下没有难送的外卖'
+            },
+            {
+                appName: '智能小二',
+                appPackage: 'yunnex.intelligencewaiter',
+                appType: '掌贝官方应用',
+                author: '掌贝',
+                crateTime: '2016-06-12',
+                slogan: '掌贝智能小二让顾客光临变得更容易'
+            },
+            {
+                appName: '商户后台',
+                appPackage: 'yunnex.saofu',
+                appType: '掌贝官方应用',
+                author: '掌贝',
+                crateTime: '2015-05-12',
+                slogan: '掌贝顾客大数据和智能营销平台'
+            }
         ]
     },
     methods: {
@@ -37,32 +116,12 @@ app = new Vue({
              activityType: this.activityType,
              activityStatus: this.activityStatus
              });*/
+        },
+        updateApp: function() {
+            alert('执行updateApp方法了!');
         }
     },
-    render: function(h) {
-        return h(yunnexTable, {
-            props: {
-                titles: {
-                    name: '姓名',
-                    sex: '性别',
-                    age: '年龄',
-                    education: '学历',
-                    job: '职业'
-                },
-                datas: [
-                    {name: '张三', sex: '男', age: 22, education: '本科', job: '金融规划师'},
-                    {name: '王立光', sex: '男', age: 30, education: '本科', job: 'java工程师'},
-                    {name: '莫婷', sex: '女', age: 22, education: '研究生', job: '学生'},
-                    {name: '李娜', sex: '女', age: 26, education: '本科', job: 'UI设计师'},
-                    {name: '李伟', sex: '男', age: 30, education: '大专', job: '销售'}
-                ],
-                isShowPagination: true,
-                pageSize: 8,
-                dataCount: 43,
-                jumpToPage: function(pageNum) {
-                    console.log('pageNum:' + pageNum);
-                }
-            }
-        });
+    components: {
+        'yunnex-table': yunnexTable
     }
 });
